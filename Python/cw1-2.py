a415910b4049ec15d76d1d747c9849d074e09d34
Miguel-124 <96996981+Miguel-124@users.noreply.github.com>
2022-03-22 12:42:47 +0100

    Create cw1-2.py

---
+++ Python/cw1-2.py
@@ -0,0 +1,22 @@
+print("Hello World\n")
+a=1
+b=1.02
+# c=2+1j*3
+d=True
+e="abc"
+c=a+b
+d=int(c)
+s=str(b)
+# z='abc'+s
+z='abc'+"123"
+cpx=2+1j*3
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(s))
+# print(type(b))
+# print(d)
+# print(s)
+# print(b)
+# print('text: '+str(z))
+print(str(cpx)+' complex\n')
